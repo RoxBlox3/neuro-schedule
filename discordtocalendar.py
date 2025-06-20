15c3e05561b064a65b222a9ba4ee7efdf2b90172
RoxBlox3 <roxblox33@proton.me>
2025-06-21 01:12:09 +0200

    Initial setup and discord bot message fetching

---
+++ discordtocalendar.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+import discord
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+load_dotenv()
+
+# Discord bot token and channel ID
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+DISCORD_CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
+
+
+@client.event
+async def on_message(message):
+    if DISCORD_CHANNEL_ID is not None and message.channel.id == int(DISCORD_CHANNEL_ID):
+        print(f"New message: {message.content}")
+        try:
+            # TODO: Add splitting message into variables and adding them to google calendar
+            pass
+        except Exception as e:
+            print(f"Failed to parse or create event: {e}")
+
+
+try:
+    if DISCORD_BOT_TOKEN is not None:
+        client.run(DISCORD_BOT_TOKEN)
+except Exception as e:
+    print(f"Failed to run bot : {e}")
+
+# async def on_ready():
+#     print(f"Logged in as {client.user}")
+#     channel = client.get_channel(DISCORD_CHANNEL_ID)
+#     if channel:
+#         messages = [message async for message in channel.history(limit=1)]
+#         if messages:
+#             latest_message=messages[0]
+#             print(f"Latest message in channel: {latest_message.content}")
