7d7153981a1f2fd09a1472b47f6f3dd6a26bd5dc
RoxBlox3 <roxblox33@proton.me>
2025-06-22 16:20:14 +0200

    Added google authentication and the creation of sleep days on the calendar

--- discordtocalendar.py
+++ discordtocalendar.py
@@ -1,10 +1,15 @@
 import os
 import re
-
 import discord
+from datetime import datetime, timezone, time
 from dotenv import load_dotenv
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 load_dotenv()
 
@@ -15,6 +20,43 @@ client = discord.Client(intents=intents)
 
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 DISCORD_CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
+SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
+CALENDAR_ID = os.getenv("CALENDAR_ID")
+
+# flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+# creds = InstalledAppFlow.from_client_secrets_file(
+#     "credentials.json", SCOPES
+# ).run_local_server(port=0)
+# service = build("calendar", "v3", credentials=creds)
+
+offline_timestamp_pattern = r"<t:(\d+):D>"
+online_timestamp_pattern = r"<t:(\d+):F>"
+
+
+def authenticate_google_calendar():
+    creds = None
+
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=8080)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
+
+
+try:
+    print("Authenticating with Google Calendar API...")
+    creds = authenticate_google_calendar()
+    service = build("calendar", "v3", credentials=creds)
+    print("Google Calendar API service initialized.")
+except Exception as e:
+    print(f"Failed to authenticate with Google Calendar API: {e}")
 
 
 @client.event
@@ -24,13 +66,38 @@ async def on_message(message):
         try:
             lines = message.content.strip().split("\n")
             removed_empty_line = []
-            for line in lines:
-                if line != "" and not line.startswith("Fanart of the week by "):
-                    removed_empty_line.append(line)
-
+            for full_line in lines:
+                # INFO: Skip lines that are empty or contain fanart of the week and ping
+                if (
+                    full_line != ""
+                    and not full_line.startswith("-# Fanart of the week by ")
+                    and not full_line.endswith(":neuroPing:")
+                ):
+                    removed_empty_line.append(full_line)
+            for line in removed_empty_line:
+                match = re.match(offline_timestamp_pattern, line)
+                if match:
+                    timestamp = int(match.group(1))
+                    message = line.split(" - ", 1)[1]
+                    event = {
+                        "summary": message,
+                        "start": {
+                            "dateTime": datetime.fromtimestamp(
+                                timestamp, tz=timezone.utc
+                            ).isoformat()
+                        },
+                        "end": {
+                            "dateTime": datetime.fromtimestamp(
+                                timestamp + 7200, tz=timezone.utc
+                            ).isoformat()
+                        },
+                    }
+                    print(timestamp)
+                    service.events().insert(
+                        calendarId=CALENDAR_ID, body=event
+                    ).execute()
+                    # TODO: Parse the data
             print(removed_empty_line)
-            # TODO: Add splitting message into variables and adding them to google calendar
-            pass
         except Exception as e:
             print(f"Failed to parse or create event: {e}")
 
