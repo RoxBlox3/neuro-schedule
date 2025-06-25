543ad43959bfa7d2b05cd39420a6f934f3ca0f0c
RoxBlox3 <RoxBlox33@proton.me>
2025-06-26 00:12:23 +0200

    Removed offline as is actually unnecessary, removed channel check, added try catch's on function call, added existing event verification

--- discordtocalendar.py
+++ discordtocalendar.py
@@ -1,14 +1,16 @@
 import os
 import re
 import discord
-from datetime import datetime, timezone, time
+from datetime import datetime, timezone
 from dotenv import load_dotenv
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google.oauth2 import service_account
+
+# from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+
+# from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 load_dotenv()
@@ -19,13 +21,11 @@ client = discord.Client(intents=intents)
 
 # environment variables
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-DISCORD_CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
 SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 CALENDAR_ID = os.getenv("CALENDAR_ID")
 
 # patterns for matching timestamps in messages
-offline_timestamp_pattern = r"<t:(\d+):D>"
-online_timestamp_pattern = r"<t:(\d+):F>"
+online_timestamp_pattern = re.compile(r"<t:(\d+):F>")
 
 
 def authenticate_google_calendar():
@@ -74,27 +74,50 @@ def insert_event(message, start_timestamp, end_timestamp):
     service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
 
 
+def event_exists(message, start_timestamp, end_timestamp):
+    # Check if an event with the same summary already exists
+    events_result = (
+        service.events()
+        .list(
+            calendarId=CALENDAR_ID,
+            timeMin=datetime.fromtimestamp(
+                start_timestamp, tz=timezone.utc
+            ).isoformat(),
+            timeMax=datetime.fromtimestamp(end_timestamp, tz=timezone.utc).isoformat(),
+            q=message,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
+    for event in events:
+        if event.get("summary") == message:
+            return True
+    return False
+
+
 @client.event
 async def on_message(message):
-    if DISCORD_CHANNEL_ID is not None and message.channel.id == int(DISCORD_CHANNEL_ID):
-        print(f"New message: {message.content}")
-        try:
-            lines = message.content.strip().split("\n")
-            for line in lines:
-                match_offline = re.match(offline_timestamp_pattern, line)
-                match_online = re.match(online_timestamp_pattern, line)
-                if match_offline:
-                    start_timestamp = int(match_offline.group(1))
-                    end_timestamp = start_timestamp + 7200
-                    message = line.split(" - ", 1)[1]
-                    insert_event(message, start_timestamp, end_timestamp)
-                if match_online:
-                    start_timestamp = int(match_online.group(1))
-                    end_timestamp = start_timestamp + 9000
-                    message = " - ".join(line.split(" - ")[2:])
-                    insert_event(message, start_timestamp, end_timestamp)
-        except Exception as e:
-            print(f"Failed to parse or create event: {e}")
+    print(f"New message: {message.content}")
+    try:
+        lines = message.content.strip().split("\n")
+        for line in lines:
+            match_online = online_timestamp_pattern.match(line)
+            if match_online:
+                start_timestamp = int(match_online.group(1))
+                end_timestamp = start_timestamp + 9000
+                split_message = " - ".join(line.split(" - ")[2:])
+                if not split_message:
+                    split_message = "No description provided"
+                if not event_exists(split_message, start_timestamp, end_timestamp):
+                    try:
+                        insert_event(split_message, start_timestamp, end_timestamp)
+                        print(f"Inserting event: {split_message}")
+                    except Exception as e:
+                        print(f"Event already exists: {e}")
+    except Exception as e:
+        print(f"Failed to parse or create event: {e}")
 
 
 try:
@@ -103,6 +126,7 @@ try:
 except Exception as e:
     print(f"Failed to run bot : {e}")
 
+# NOTE: Could be used to check if last message is the same as the new one
 # async def on_ready():
 #     print(f"Logged in as {client.user}")
 #     channel = client.get_channel(DISCORD_CHANNEL_ID)
@@ -110,4 +134,4 @@ except Exception as e:
 #         messages = [message async for message in channel.history(limit=1)]
 #         if messages:
 #             latest_message=messages[0]
-#             print(f"Latest message in channel: {latest_message.content}")
+#        vc     print(f"Latest message in channel: {latest_message.content}")
